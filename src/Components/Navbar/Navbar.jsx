7a4b3cd4103d594c42ede96607688f373dfcace6
zulfiqaraliwd <zulfiqaraliwd@gmail.com>
2026-07-26 14:50:54 +0500

    initialize the react app for my porfolio

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>About</li>
+      <li>Contact</li>
+      <li>services</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
